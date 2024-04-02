0fcb1f55ed7994ec06554503823a59684f3f669f
Simran <98958500+SimranBanwait@users.noreply.github.com>
2024-04-02 06:31:02 +0530

    Update exp2.sh

--- exp2.sh
+++ exp2.sh
@@ -2,7 +2,7 @@
 
 #USAGE : ./PITR.sh table-name1 table-name2 table-name3 
 #USAGE : make sure the region is correct
-for table in "$@"
+for table in "$@" 
 do
   aws dynamodb update-continuous-backups \
     --table-name "$table" \
